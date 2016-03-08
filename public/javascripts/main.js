4b32dd2a23abe42d24cd41115ee9b86436bb0c26
Zarin Bhuiyan <zarin.bhuiyan@students.olin.edu>
2016-03-08 00:51:50 -0500

    Other changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -25,107 +25,92 @@ function mainController($scope, $http) {
       });
   };
 
-  $scope.selectWiki = function(header){
-
-    $scope.showNewWiki = false; 
-    $scope.showEdit = true;
-    // $scope.showContent = true; 
-    $scope.newWiki = {};
-
-    console.log('in selectWiki')
-    $http.get('api/header/'+header)
-      .success(function(data){
-        $scope.mainWiki = data;
-        $scope.header = $scope.mainWiki.header;
-        $scope.content = $scope.mainWiki.content;
-        $scope.editorEnabled = false;
-        console.log(data)
-      })
-      .error(function(data){
-        console.log('Error:' + data);
-      });
+  $scope.likeHipHop = function(){
+    console.log('Im in likehiphop');
   };
 
-  $scope.saveNewWiki = function(){ 
-    console.log("are you in here (in new wiki)??")
-    $scope.showNewWiki = false;
-    // $scope.showContent = false;  
-
-    console.log("about to post")
-    $http.post('/api/createNew', {header: $scope.newWiki.header, content: $scope.newWiki.content})
-      .success(function(data){ 
-        $scope.newWiki = data.newWiki;
-        $scope.wiki = data.all;
-        console.log("Success", data.newWiki);
-        console.log("data all ", data.all);
-      })
-      .error(function(data){ 
-        console.log("Failure", data)
-      })
-
-  }
-      
-  $scope.enableShowNewWiki = function(){ 
-    console.log($scope.showNewWiki); 
-    $scope.showNewWiki = true; 
-    console.log($scope.showNewWiki);
-  }
-
-    $scope.enableEditor = function() {
-        $scope.editorEnabled = true;
-        $scope.editableHeader = $scope.mainWiki.header;
-        $scope.editableContent = $scope.mainWiki.content;
-    };
-      
-    $scope.disableEditor = function() {
-        $scope.editorEnabled = false;
-    };
-      
-    $scope.save = function(header) {
-        $scope.showNewWiki = false; 
-        // $scope.showContent = true; 
-        $scope.mainWiki.content = $scope.editableContent;
-        $scope.mainWiki.header = $scope.editableHeader;
-        $scope.disableEditor();
-        console.log($scope.mainWiki.content, 'new content')
-        console.log($scope.mainWiki.header, 'new header')
-
-        $http.post('/api/header/' + header, {header: $scope.mainWiki.header, content:$scope.mainWiki.content})
-          .success(function(data){
-            $scope.wiki = data.all;
-            console.log(data)
-            console.log('saved correctly')
-
-          })
-          .error(function(data){
-          console.log('Error:' + data);
-        });
-    };
-
-    $scope.partialSearch = function(header){
-
-    $scope.showNewWiki = false; 
-    // $scope.showContent = true; 
-    $scope.newWiki = {};
-
-    console.log('in selectWiki')
-    $http.get('api/header/'+header)
-      .success(function(data){
-        $scope.mainWiki = data;
-        $scope.header = $scope.mainWiki.header;
-        $scope.content = $scope.mainWiki.content;
-        $scope.editorEnabled = false;
-        console.log(data)
-      })
-      .error(function(data){
-        console.log('Error:' + data);
-      });
+  $scope.likeIndieAlt = function(){
+    console.log('im in likeindiealt');
   };
 
-    $scope.search = function(){
-      console.log('im in search');
-      $scope.searchQuery = angular.copy($scope.query)
-      $scope.selectWiki($scope.searchQuery);
-    }
+  $scope.likePop = function(){
+    console.log('im in likepop');
+  };
+
+  $scope.likeRock = function(){
+    console.log('im in likerock');
+  };
+
+  $scope.likeCountry = function(){
+    console.log('im in likecountry');
+  };
+
+/*
+  $scope.enableLikeHopHop = function(){ 
+   
+  }*/
+      
+//   $scope.enableShowNewWiki = function(){ 
+//     console.log($scope.showNewWiki); 
+//     $scope.showNewWiki = true; 
+//     console.log($scope.showNewWiki);
+//   }
+
+//     $scope.enableEditor = function() {
+//         $scope.editorEnabled = true;
+//         $scope.editableHeader = $scope.mainWiki.header;
+//         $scope.editableContent = $scope.mainWiki.content;
+//     };
+      
+//     $scope.disableEditor = function() {
+//         $scope.editorEnabled = false;
+//     };
+      
+//     $scope.save = function(header) {
+//         $scope.showNewWiki = false; 
+//         // $scope.showContent = true; 
+//         $scope.mainWiki.content = $scope.editableContent;
+//         $scope.mainWiki.header = $scope.editableHeader;
+//         $scope.disableEditor();
+//         console.log($scope.mainWiki.content, 'new content')
+//         console.log($scope.mainWiki.header, 'new header')
+
+//         $http.post('/api/header/' + header, {header: $scope.mainWiki.header, content:$scope.mainWiki.content})
+//           .success(function(data){
+//             $scope.wiki = data.all;
+//             console.log(data)
+//             console.log('saved correctly')
+
+//           })
+//           .error(function(data){
+//           console.log('Error:' + data);
+//         });
+//     };
+
+//     $scope.partialSearch = function(header){
+
+//     $scope.showNewWiki = false; 
+//     // $scope.showContent = true; 
+//     $scope.newWiki = {};
+
+//     console.log('in selectWiki')
+//     $http.get('api/header/'+header)
+//       .success(function(data){
+//         $scope.mainWiki = data;
+//         $scope.header = $scope.mainWiki.header;
+//         $scope.content = $scope.mainWiki.content;
+//         $scope.editorEnabled = false;
+//         console.log(data)
+//       })
+//       .error(function(data){
+//         console.log('Error:' + data);
+//       });
+//   };
+
+//     $scope.search = function(){
+//       console.log('im in search');
+//       $scope.searchQuery = angular.copy($scope.query)
+//       $scope.selectWiki($scope.searchQuery);
+//     }
 
 }
