aa293980772c50a40005a29ea438ac7a94799e59
Zarin Bhuiyan <zarin.bhuiyan@students.olin.edu>
2016-03-11 01:03:15 -0500

    Added all genres, fixed more css

--- public/js/controllers/userController.js
+++ public/js/controllers/userController.js
@@ -3,6 +3,10 @@
 app.controller('userController', function($scope, spotinderService, Spotify) {
 
 	$scope.hip_hop_genre = [];
+	$scope.pop_genre = [];
+	$scope.rock_genre = [];
+	$scope.country_genre = [];
+	$scope.alternative_genre = [];
 
 
 
@@ -13,8 +17,54 @@ app.controller('userController', function($scope, spotinderService, Spotify) {
 		name = track['name'];
 		artist = track['artists'][0]['name'];
 		$scope.hip_hop_genre.push( { 'genre': 'hip_hop', 'name':name, "artist":artist } );
-		// $scope.hip_hop_genre = $scope.hip_hop_genre(3);
-		console.log("number",$scope.hip_hop_genre.length);
+		$scope.hip_hop_genre = $scope.hip_hop_genre.slice(0,3);
+		// console.log("number",$scope.hip_hop_genre.length);
+      });
+    });
+
+
+    Spotify.search('pop', 'track').then(function (data) {
+      console.log(data);
+      var trakArray = data['tracks']['items'];
+      trakArray.forEach(function(track) {
+		name = track['name'];
+		artist = track['artists'][0]['name'];
+		$scope.pop_genre.push( { 'genre': 'pop', 'name':name, "artist":artist } );
+		$scope.pop_genre = $scope.pop_genre.slice(0,3);
+		
+      });
+    });
+
+      Spotify.search('rock', 'track').then(function (data) {
+      console.log(data);
+      var trakArray = data['tracks']['items'];
+      trakArray.forEach(function(track) {
+		name = track['name'];
+		artist = track['artists'][0]['name'];
+		$scope.rock_genre.push( { 'genre': 'rock', 'name':name, "artist":artist } );
+		$scope.rock_genre = $scope.rock_genre.slice(0,3);
+      });
+    });
+
+      Spotify.search('country', 'track').then(function (data) {
+      console.log(data);
+      var trakArray = data['tracks']['items'];
+      trakArray.forEach(function(track) {
+		name = track['name'];
+		artist = track['artists'][0]['name'];
+		$scope.country_genre.push( { 'genre': 'rock', 'name':name, "artist":artist } );
+		$scope.country_genre = $scope.country_genre.slice(0,3);
+      });
+    });
+
+      Spotify.search('alternative', 'track').then(function (data) {
+      console.log(data);
+      var trakArray = data['tracks']['items'];
+      trakArray.forEach(function(track) {
+		name = track['name'];
+		artist = track['artists'][0]['name'];
+		$scope.alternative_genre.push( { 'genre': 'rock', 'name':name, "artist":artist } );
+		$scope.alternative_genre = $scope.alternative_genre.slice(0,3);
       });
     });
 
