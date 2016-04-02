b0fe3b7c1de917e6085c37c4a19386aeb90dbd4b
tangtangwzc <tangtangwzc@gmail.com>
2016-04-02 15:36:05 -0400

    debugging

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('*', function (req, res) {
 });
 
 // START SERVER ================================================================
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 8888;
 app.listen(PORT, function() {
   console.log("Topics running on port:", PORT);
 });
